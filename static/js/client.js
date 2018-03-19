72c96b895ec6ee643c8b080f98a52812393457ac
Vladimir Fashchevskiy <ghost28147@gmail.com>
2018-03-20 01:26:04 +1000

    Клиент для общения с сервером.

---
+++ static/js/client.js
@@ -0,0 +1,35 @@
+TodoClient = function() {};
+
+TodoClient.prototype.endpoint = '/api/items';
+
+TodoClient.prototype.removeItem = function (id) {
+    return jQuery.ajax({
+            url: this.endpoint + '?id=' + id,
+            method: "DELETE"
+        }
+    );
+};
+
+TodoClient.prototype.toggleItem = function (id) {
+    return jQuery.ajax({
+            url: this.endpoint + '?state=toggle&id=' + id,
+            method: "PATCH"
+        }
+    );
+};
+
+TodoClient.prototype.removeCompleted = function (id) {
+    return jQuery.ajax({
+            url: this.endpoint + '/completed',
+            method: "DELETE"
+        }
+    );
+};
+
+TodoClient.prototype.markAll = function (yes) {
+    return jQuery.ajax({
+            url: this.endpoint + '?state=' + (yes? 'active' : 'completed'),
+            method: "PATCH"
+        }
+    );
+};
